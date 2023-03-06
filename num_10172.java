3032a2874c26ad2016a8a1a51402a2a39d453cca
Java-Baekjoon <dlwjdghk133@naver.com>
2023-03-06 18:25:33 +0900

    강아지 출력

---
+++ num_10172.java
@@ -0,0 +1,11 @@
+public class num_10172 {
+    public static void main(String[] args) {
+
+        System.out.print (
+                        "|\\_/|\n" +
+                        "|q p|   /}\n" +
+                        "( 0 )\"\"\"\\\n" +
+                        "|\"^\"`    |\n" +
+                        "||_/=\\\\__|");
+    }
+}
