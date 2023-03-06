46732c67527802ac6a216bd9e42a0d1b5ae550b5
Java-Baekjoon <dlwjdghk133@naver.com>
2023-03-06 18:06:23 +0900

    입력 받아서 ??!

---
+++ num_10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class num_10926 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String last = "??!";
+
+        String s = in.next();
+        System.out.println(s + last);
+    }
+}
