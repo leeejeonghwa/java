5177116952679b76719fde26f0c13ecac426b1e3
Java-Baekjoon <dlwjdghk133@naver.com>
2023-03-06 18:15:08 +0900

    입력 받아서 a+b+c 출력

---
+++ num_11382.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class num_11382 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner (System.in);
+
+        long A = in.nextLong();
+        long b = in.nextLong();
+        long c = in.nextLong();
+
+        System.out.println(A + b + c);
+    }
+}
